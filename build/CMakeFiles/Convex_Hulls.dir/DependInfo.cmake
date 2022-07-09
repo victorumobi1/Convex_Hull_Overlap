1c51ff355760916d8b0281f780def8d78b0162d5
Victor Umobi <83788917+victorumobi1@users.noreply.github.com>
2022-07-09 16:41:04 +0100

    Add files via upload

---
+++ build/CMakeFiles/Convex_Hulls.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/victorumobi/Desktop/convex_hulls-main/lib/geometry/line.cpp" "CMakeFiles/Convex_Hulls.dir/lib/geometry/line.cpp.o" "gcc" "CMakeFiles/Convex_Hulls.dir/lib/geometry/line.cpp.o.d"
+  "/Users/victorumobi/Desktop/convex_hulls-main/lib/geometry/line_segment.cpp" "CMakeFiles/Convex_Hulls.dir/lib/geometry/line_segment.cpp.o" "gcc" "CMakeFiles/Convex_Hulls.dir/lib/geometry/line_segment.cpp.o.d"
+  "/Users/victorumobi/Desktop/convex_hulls-main/lib/geometry/point.cpp" "CMakeFiles/Convex_Hulls.dir/lib/geometry/point.cpp.o" "gcc" "CMakeFiles/Convex_Hulls.dir/lib/geometry/point.cpp.o.d"
+  "/Users/victorumobi/Desktop/convex_hulls-main/lib/geometry/polygon.cpp" "CMakeFiles/Convex_Hulls.dir/lib/geometry/polygon.cpp.o" "gcc" "CMakeFiles/Convex_Hulls.dir/lib/geometry/polygon.cpp.o.d"
+  "/Users/victorumobi/Desktop/convex_hulls-main/lib/geometry/ray.cpp" "CMakeFiles/Convex_Hulls.dir/lib/geometry/ray.cpp.o" "gcc" "CMakeFiles/Convex_Hulls.dir/lib/geometry/ray.cpp.o.d"
+  "/Users/victorumobi/Desktop/convex_hulls-main/src/main.cpp" "CMakeFiles/Convex_Hulls.dir/src/main.cpp.o" "gcc" "CMakeFiles/Convex_Hulls.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
